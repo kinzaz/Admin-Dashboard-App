8cb533f3c1d14ccc021b5fe6dfa94bb96d55813c
Vladislav Vlasenko <kinzazdev579@gmail.com>
2023-05-07 14:27:03 +0300

    calendar page

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -5,12 +5,12 @@ import Team from './scenes/team';
 import Contacts from './scenes/contacts';
 import Invoices from './scenes/invoices';
 import Form from './scenes/form';
+import Calendar from './scenes/calendar';
 // import Line from './scenes/line';
 // import Bar from './scenes/bar';
 // import Pie from './scenes/pie';
 // import FAQ from './scenes/faq';
 // import Geography from './scenes/geography';
-// import Calender from './scenes/Calender';
 import { Route, Routes } from 'react-router-dom';
 import MainProvider from './providers/MainProvider';
 
@@ -27,11 +27,11 @@ function App() {
 						<Route path="/contacts" element={<Contacts />} />
 						<Route path="/invoices" element={<Invoices />} />
 						<Route path="/form" element={<Form />} />
+						<Route path="/calendar" element={<Calendar />} />
 						{/* <Route path="/bar" element={<Bar />} /> */}
 						{/* <Route path="/line" element={<Line />} /> */}
 						{/* <Route path="/faq" element={<FAQ />} /> */}
 						{/* <Route path="/geography" element={<Geography />} /> */}
-						{/* <Route path="/calender" element={<Calender />} /> */}
 						{/* <Route path="/pie" element={<Pie />} /> */}
 					</Routes>
 				</main>
